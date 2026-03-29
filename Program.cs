e1d6ab696cd67407bc02d8f90901693d5f3a5bbd
Sofia <simanihuruksofia@gmail.com>
2026-03-29 13:14:05 +0700

    menambahkan class DataGeneric

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class DataGeneric<T>
+{
+    private T data;
+    public DataGeneric(T data)
+    {
+        this.data = data;
+    }
+
+    public void PrintData()
+    {
+        Console.WriteLine("Data yang tersimpan adalah: " + data);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataGeneric<string> data = new DataGeneric<string>("103022400084");
+        data.PrintData();
+    }
+}
